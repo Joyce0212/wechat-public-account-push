290dd4df6d55a51bf3169a8ff54af759617a63de
Joyce0212 <115612432+Joyce0212@users.noreply.github.com>
2022-10-12 15:43:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'opmfL5-i_Grqx2eS2556_78A9aBc','opmfL57r5HTRj9thqiI0fx-De3Ts',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'XsbM9vFzj8nmn27aSNY99bxTUdrxUqS-2wEtUp7Zfho',
+      useTemplateId: '	NpfsWHjzvpix0C9xBzb57Z4PJqUsqOnHGlkWQmih4-g',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-25',
       festivals: [
@@ -46,7 +46,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'XsbM9vFzj8nmn27aSNY99bxTUdrxUqS-2wEtUp7Zfho',
+  CALLBACK_TEMPLATE_ID: 'fwSmmEmWUipU1c2V8IIzddFcjRwjhgG2tmmqD7aLLsE',
 
   CALLBACK_USERS: [
     {
